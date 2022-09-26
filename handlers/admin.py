23941554d9ca99e659e406b615ed8b4b5c0449d8
hazadus <hazadus7@gmail.com>
2022-09-27 00:44:29 +0300

    Cancel command (for admin mode) added.

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,5 +1,6 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
@@ -10,9 +11,17 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
-async def admin_start(message: types.Message):
+async def admin_start(message: types.Message):  # TODO: check if user is admin via os.getenv('BOT_ADMIN')
     await FSMAdmin.photo.set()
     await message.answer('Upload photo')
+
+
+async def admin_cancel(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.answer('OK.')
 
 
 async def set_photo(message: types.Message, state: FSMContext):
@@ -48,6 +57,9 @@ async def set_price(message: types.Message, state: FSMContext):
 
 def register_admin_handlers(disp: Dispatcher):
     disp.register_message_handler(admin_start, commands=['upload'], state=None)
+    disp.register_message_handler(admin_cancel, commands=['отмена', 'cancel'], state='*')
+    disp.register_message_handler(admin_cancel, Text(equals='отмена', ignore_case=True), state='*')
+    disp.register_message_handler(admin_cancel, Text(equals='cancel', ignore_case=True), state='*')
     disp.register_message_handler(set_photo, content_types=['photo'], state=FSMAdmin.photo)
     disp.register_message_handler(set_name, state=FSMAdmin.name)
     disp.register_message_handler(set_description, state=FSMAdmin.desctiption)
